2ba21fa0b5ddc270b600780e356f2c10b8c44bc7
haero77 <shineecard5@gmail.com>
2023-12-22 18:44:48 +0900

    initial commit

---
+++ src/test/java/com/fc/commerce/CommerceApplicationTests.java
@@ -0,0 +1,22 @@
+package com.fc.commerce;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		System.out.println("stse");
+	}
+
+
+}
