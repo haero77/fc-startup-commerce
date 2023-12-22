cb9ce7e4809ad2a1c0e47296ac5e806c716e0df8
haero77 <shineecard5@gmail.com>
2023-12-22 20:51:32 +0900

    chore: intentional fail test for github actions test

--- src/test/java/com/fc/commerce/domain/repository/MemberRepositoryTest.java
+++ src/test/java/com/fc/commerce/domain/repository/MemberRepositoryTest.java
@@ -1,31 +1,35 @@
 package com.fc.commerce.domain.repository;
 
-import com.fc.commerce.common.support.RepositoryTestSupport;
-import com.fc.commerce.domain.entity.Member;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.fc.commerce.common.support.RepositoryTestSupport;
+import com.fc.commerce.domain.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 class MemberRepositoryTest extends RepositoryTestSupport {
 
-	@Autowired
-	MemberRepository sut;
+    @Autowired
+    MemberRepository sut;
 
-	@Test
-	void findByName() {
-		String name = "name1";
-		Member member = Member.ofName(name);
-		sut.save(member);
+    @Test
+    void findByName() {
+        String name = "name1";
+        Member member = Member.ofName(name);
+        sut.save(member);
 
-		Member actual = sut.findByName(name).get();
+        Member actual = sut.findByName(name).get();
 
-		assertAll(
-			() -> assertThat(actual).isEqualTo(member),
-			() -> assertThat(actual.getName()).isEqualTo(name)
-		);
-	}
+        assertAll(
+                () -> assertThat(actual).isEqualTo(member),
+                () -> assertThat(actual.getName()).isEqualTo(name)
+        );
+    }
+
+    @Test
+    void failTest() {
+        assertThat(true).isEqualTo(false);
+    }
 
 }
